c40d26215d56174eb24dcdf55b53932607d2ba74
edugdl <eduardogdelazari2005@gmail.com>
2023-11-15 20:39:10 -0300

    Inserindo o debug padrao

--- trabalhoIII/fs.cc
+++ trabalhoIII/fs.cc
@@ -25,7 +25,15 @@ int INE5412_FS::fs_format()
 
 void INE5412_FS::fs_debug()
 {
+	union fs_block block;
 
+	disk->read(0, block.data);
+
+	cout << "superblock:\n";
+	cout << "    " << (block.super.magic == FS_MAGIC ? "magic number is valid\n" : "magic number is invalid!\n");
+ 	cout << "    " << block.super.nblocks << " blocks\n";
+	cout << "    " << block.super.ninodeblocks << " inode blocks\n";
+	cout << "    " << block.super.ninodes << " inodes\n";
 }
 
 int INE5412_FS::fs_mount()
