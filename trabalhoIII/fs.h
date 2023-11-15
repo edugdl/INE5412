deec43918f18b55bb13981e37a8621a0e912a464
Eduardo g de lazari <90356149+edugdl@users.noreply.github.com>
2023-11-15 20:40:31 -0300

    Update fs.h

--- trabalhoIII/fs.h
+++ trabalhoIII/fs.h
@@ -57,6 +57,7 @@ public:
     int  fs_read(int inumber, char *data, int length, int offset);
     int  fs_write(int inumber, const char *data, int length, int offset);
 
+
 private:
     Disk *disk;
 };
