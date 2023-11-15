7341846b9ce53babc1b0d81c8a3e11730c207252
edugdl <eduardogdelazari2005@gmail.com>
2023-11-15 15:34:45 -0300

    Formatar disco implementado

--- trabalhoIII/fs.cc
+++ trabalhoIII/fs.cc
@@ -2,7 +2,31 @@
 
 int INE5412_FS::fs_format()
 {
-	return 0;
+	union fs_block super_block;
+	union fs_block inode_block;
+	union fs_block block;
+	
+	disk->read(0, super_block.data);
+	for (int i = 1; i <= super_block.super.ninodeblocks; i++) {
+		disk->read(i, inode_block.data);
+		for (int j = 0; j < INODES_PER_BLOCK; j++) {
+			inode_block.inode[j].isvalid = 0;
+			for (int k = 0; k < POINTERS_PER_INODE; k++) inode_block.inode[j].direct[k] = 0;
+			inode_block.inode[j].indirect = 0;
+			inode_block.inode[j].size = 0; // Verificar se essa linha funciona corretamente
+		}
+		disk->write(i, inode_block.data);
+	}
+
+	for (int i = super_block.super.ninodeblocks + 1; i < super_block.super.nblocks; i++) {
+		disk->read(i, block.data);
+		for (long unsigned j = 0; j < sizeof(block.data); j++) block.data[j] = '0';
+		disk->write(i, block.data);
+	}
+
+	super_block.super.ninodes = 0;
+
+	return 1;
 }
 
 void INE5412_FS::fs_debug()
